14ebba2e3ffc07c2e25a09ab2c47b046fc5bb7d8
VictorFCh <vfariachavez@gmail.com>
2020-12-30 19:33:33 +0000

    interfaz con bem

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import classNames from 'classnames';
+import './App.css';
 
+const wishes = [
+  {
+    text: 'Learn japanese',
+    done: false,
+  },
+  {
+    text: 'Do some exercise',
+    done: true,
+  },
+  {
+    text: 'Go to the mall',
+    done: false,
+  },
+];
 const App = () => (
-  <div>
+  <div className="app">
     <h1>My wishlist app</h1>
-    <fieldset>
-      <legend>New wish</legend>
-      <input type="text" placeholder="Enter your wish here" />
+    <fieldset className="wish-input">
+      <legend className="wish-input__label">New wish</legend>
+      <input className="wish-input__field" type="text" placeholder="Enter your wish here" />
     </fieldset>
+    <ul className="wish-list">
+      {wishes.map(({ text, done }, i) => (
+        <li key="text" className={classNames('wish-list__item', { 'wish-list__item--done': done })}>
+          <input type="checkbox" id={`wish${i}`} checked={done} />
+          <label htmlFor={`wish${i}`}>{text}</label>
+        </li>
+      ))}
+    </ul>
+    <button type="button" className="wish-clear">Archive done</button>
   </div>
 );
 
