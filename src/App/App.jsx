a7efda1d3ad52f0cb17b6f4db44d0edb61b44baa
VictorFCh <vfariachavez@gmail.com>
2020-12-30 18:29:50 +0000

    first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>My wishlist app</h1>
+    <fieldset>
+      <legend>New wish</legend>
+      <input type="text" placeholder="Enter your wish here" />
+    </fieldset>
+  </div>
+);
+
+export default App;
